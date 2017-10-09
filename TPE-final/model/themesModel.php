b28c8632ef16a2c95e129a92e3866b050864766c
Sebastian Pellitero <sebastian.pellitero@gmail.com>
2017-10-09 02:38:11 -0300

    Not working themes

--- TPE-final/model/themesModel.php
+++ TPE-final/model/themesModel.php
@@ -3,7 +3,7 @@
 class themesModel extends model {
   function getThemes(){
   	
-    $sentencia = $this->db->prepare("select * from categoria");
+    $sentencia = $this->db->prepare("select * from imagen, categoria where imagen.id_categoria = categoria.id_categoria order by categoria.id_categoria");
     $sentencia->execute();
     return $sentencia->fetchAll(PDO::FETCH_ASSOC);
   }
